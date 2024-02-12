6f54c486812edb768617d4c3bdc4a6621fc5d642
jose.macias <jose.macias@klym.com>
2024-02-12 23:34:14 +0800

    add limited rate endpoints

--- flask_basic_api/app/routes/api_routes.py
+++ flask_basic_api/app/routes/api_routes.py
@@ -12,3 +12,11 @@ def data():
 @api_routes.route('/test', methods=['GET'])
 def test():
     return "This is a test!!"
+
+@api_routes.route('/unlimited', methods=['GET'])
+def unlimited():
+    return "Unlimited! Let's Go!"
+
+@api_routes.route('/limited', methods=['GET'])
+def limited():
+    return "Limited, don't over use me!"
